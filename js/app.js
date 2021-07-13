b87a855ea826e6183ef7e323cea6730023f99f75
Stephanie Liu <yliu@Stephanies-MacBook-Air.local>
2021-07-13 11:23:07 -0500

    app.js

--- js/app.js
+++ js/app.js
@@ -2,10 +2,18 @@ const $projects = $(".projects");
 $.ajax("./json/projects.json")
 .then(data => {
     data.forEach(project => {
-        const $project = $("#projects");
-        const $title = $("<h2>").addClass("name").text(`${project.name}`);
-        const $description = $("<h3>").addClass("description").text(`${project.description}`);
-        $project.append($title).append($description);
+        // const $project = $("#projects");
+        // const $title = $("<h2>").addClass("name").text(`${project.name}`);
+        // const $description = $("<h3>").addClass("description").text(`${project.description}`);
+        // $project.append($title).append($description);
+        const project = $(`<div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>`)
         $projects.append($project);
     })
 })
